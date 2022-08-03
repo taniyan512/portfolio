e16f8b58cc2f5d0373782890fe4c58ecc83a65ff
谷田匠 <tanidatakumi@tanitakunettowaku.local>
2022-08-04 07:51:34 +0900

    pumaを起動した際にソケットを作成する位置を指定

--- config/puma.rb
+++ config/puma.rb
@@ -1,6 +1,3 @@
-#必要なさそうであれば削除する
-worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
-
 max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
