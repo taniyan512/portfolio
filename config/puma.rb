e208ddc86d8ce535f54fa0da0bf0499bf33500f1
谷田匠 <tanidatakumi@tanitakunettowaku.local>
2022-08-11 09:08:27 +0900

    [change]AWSにデプロイするためソケットを作成する位置を指定

--- config/puma.rb
+++ config/puma.rb
@@ -1,13 +1,10 @@
-max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
-min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
-threads min_threads_count, max_threads_count
-
-
-port ENV.fetch("PORT") { 3000 }
-
-bind "unix:///var/www/portfolio/tmp/sockets/puma.sock"
+threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }.to_i
+threads threads_count, threads_count
+port        ENV.fetch("PORT") { 3000 }
 environment ENV.fetch("RAILS_ENV") { "development" }
-
-pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
-
 plugin :tmp_restart
+
+app_root = File.expand_path("../..", __FILE__)
+bind "unix://#{app_root}/tmp/sockets/puma.sock"
+
+stdout_redirect "#{app_root}/log/puma.stdout.log", "#{app_root}/log/puma.stderr.log", true
