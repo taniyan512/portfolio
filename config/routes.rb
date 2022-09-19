6212f670dc7d6d85751ddca3ea4f4bb6fda978cb
谷田匠 <tanidatakumi@tanitakunettowaku.local>
2022-09-19 15:49:27 +0900

    [add]refs #2 プロフィール画面作成につき実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+
   get '/' => 'home#top'
+  get 'home/profile' => 'home#profile', as: :profile_home
+  post 'home/profile' => 'home#update', as: :update_home
 end
