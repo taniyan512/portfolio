717a8390101b79c87a1410e842fca384c361d813
谷田匠 <tanidatakumi@tanitakunettowaku.local>
2022-09-24 06:17:49 +0900

    [add] refs #14 userが親、serviceが子のリレーションを実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_23_063232) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_23_102008) do
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -45,6 +45,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_23_063232) do
     t.integer "lesson_time"
     t.integer "lesson_price"
     t.string "lesson_detail"
+    t.integer "user_id"
   end
 
   create_table "users", charset: "utf8", force: :cascade do |t|
