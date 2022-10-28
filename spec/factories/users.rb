3903f1f19599ed2791944dfb56f19325613701b1
谷田匠 <tanidatakumi@tanitakunettowaku.local>
2022-10-28 21:37:22 +0900

    [add] refs #18 userモデルのテストを実装

---
+++ spec/factories/users.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :user do
+    email              {"te@st"}
+    password           {"123456"}
+    user_name          {"test"}
+    self_introduce     {"test"}
+  end
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,54 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'ユーザー登録' do
+    it 'email,passwordが存在すれば登録出来ること' do
+      user = build(:user)
+      expect(user).to be_valid
+    end
+    
+    it 'emailが空欄の時、登録できないこと' do
+      user = build(:user)
+      user.email = ''
+      expect(user).to be_invalid
+    end
+    
+    it 'passwordが空欄の時、登録できないこと' do
+      user = build(:user)
+      user.password = ''
+      expect(user).to be_invalid
+    end
+    
+  end
+
+  describe 'プロフィール登録' do
+    it 'user_nameが存在すれば登録出来ること' do
+      user = build(:user)
+      expect(user).to be_valid
+    end
+
+    it 'user_nameが空欄の時、登録できないこと' do
+      user = build(:user)
+      user.user_name = ''
+      expect(user).to be_invalid
+    end
+
+  end
+
+  describe 'リレーション' do
+    it 'userモデルを削除したとき、serviceモデルも削除されること' do
+      user = build(:user)
+      user.services << build(:service)
+      user.save
+      expect{ user.destroy }.to change{ Service.count }.by(-1)
+    end
+
+    it 'userモデルを削除したとき、movieモデルも削除されること' do
+      user = build(:user)
+      user.movies << build(:movie)
+      user.save
+      expect{ user.destroy }.to change{ Movie.count }.by(-1)
+    end
+  end
+
+end
